34809b10bae49e4285ef79c63a212c548bd0d3d9
jgvdangelo <jgvdangelo@gmail.com>
2016-11-18 21:54:41 -0500

    Solved another problem

--- src/bitManipulation/BitManProblems.java
+++ src/bitManipulation/BitManProblems.java
@@ -1,5 +1,7 @@
 package bitManipulation;
 
+import java.util.*;
+
 public class BitManProblems {
 	
 	// Common bit operations, for my own practice
@@ -35,7 +37,60 @@ public class BitManProblems {
 		return output;
 	}
 	
+	// Problem 5.3
+	public static int flipBitToWin(int n) {
+		List<Integer> oneZeros = new LinkedList<Integer>();
+		
+		int count = 0; int currVal;
+		boolean countZeros = true;
+		while (n != 0) {
+			currVal = n & 1;
+			if ((currVal == 0 && countZeros) || (currVal == 1 && !countZeros)) {
+				count++;
+			} else {
+				oneZeros.add(count);
+				count = 1;
+				countZeros = !countZeros;
+			}
+			n >>>= 1;
+		}
+		
+		oneZeros.add(count);
+		int curr = 0;
+		int max = 0;
+		int left; int right;
+		for (int i = 0; i < oneZeros.size(); i += 2) {
+			if (i > 0) {
+				left = oneZeros.get(i - 1);
+			} else {
+				left = 0;
+			}
+			
+			if (i + 1 < oneZeros.size()) {
+				right = oneZeros.get(i + 1);
+			} else {
+				right = 0;
+			}
+			
+			if (oneZeros.get(i) == 1) {
+				curr = left + right + 1;
+			} else if (oneZeros.get(i) > 1){
+				curr = Math.max(left, right) + 1;
+			}
+			if (curr > max) {
+				max = curr;
+			}
+		}
+		
+		
+		System.out.println(oneZeros);
+		
+		return max;
+	}
+	
 	public static void main(String[] args) {
-		int u = insertion(-100000000, 15, 3, 6);
+		int num = -3;
+		System.out.println(Integer.toBinaryString(num));
+		System.out.println(flipBitToWin(num));
 	}
 }
